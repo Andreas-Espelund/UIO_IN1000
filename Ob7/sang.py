29cc7a258cb71e85140d5e2a9cf55c54aece39d2
Andreas-Espelund <andreres@uio.no>
2020-10-10 21:31:33 +0200

    Lagt til kommentar på klassene

--- Ob7/sang.py
+++ Ob7/sang.py
@@ -1,16 +1,26 @@
+'''
+Klassedefinisjon for Sang:
+
+Klassen har instansvariablene _tittel og _artist, og 
+metodene spill, sjekkArtist, sjekkTittel og sjekkArtistOgTittel. 
+'''
+
 class Sang:
+    #sang objektene blir initialisert med _artist og _tittel som instansvariabler
     def __init__(self,artist,tittel):
         self._tittel = tittel.lower()
         self._artist = artist.lower()
-
+    #__str__ returnerer artistnavn og låtnavn på en lesbar måte
     def __str__(self):
         out = self._tittel+", "+self._artist
         return out
 
+    #spill metoden returnerer en streng som sier at sangen blir spilt av.
     def spill(self):
          out = "Spiller "+self._tittel+" av "+self._artist+"."
          return out
 
+    #sjekkArtist tar et navn som parameter og sjekker om navnet eller et av navnene i navn, samsvarer med artistnavnet
     def sjekkArtist(self,navn):
         status = False
         inputListe = navn.split(" ")
@@ -20,7 +30,8 @@ class Sang:
                 status = True
 
         return status
-
+    
+    #sjekkTittel tar en låttittel som parameter og sjekker om det stemmer med låttittelen til objektet
     def sjekkTittel(self,tittel):
         status = False
         inputListe = tittel.split(" ")
@@ -31,6 +42,7 @@ class Sang:
 
         return status
 
-
+    #sjekkArtistOgTittel kaller på sjekttTittel og sjekkArtist og returerer True om begge er True
+    #og False dersom begge er False 
     def sjekkArtistOgTittel(self,artist,tittel):
         return self.sjekkTittel(tittel) and self.sjekkArtist(artist)
--- Ob7/spilleliste.py
+++ Ob7/spilleliste.py
@@ -1,18 +1,31 @@
+'''
+Klassedefinisjon for Spilleliste
+
+Klassen har instansvariablene _sanger og _navn
+Klassens metoder er: lesFraFil, leggTilSanger,fjernSang,
+fjernSang,finnSang,spillSang,spillAlle,hentArtistUtvalg.
+'''
+
+#jeg importerer klassen Sang
 from sang import Sang
 
 class Spilleliste:
+    #Spilleliste-objektene blir initialistert med _navn og _sanger, som er en tom liste.
     def __init__(self, listenavn):
         self._sanger = []
         self._navn = listenavn
-
+    
+    #__str__ funksjonen returnerer self._navn og self._sanger
     def __str__(self):
         output = self._navn+" :"+self._sanger
         return output
 
+    #lesFraFIl tar et filnavn som parameter og leser inn linjene med artistnavn og tittel
     def lesFraFil(self,filnavn):
         sanger ={}
         i = 0
         fil = open(filnavn)
+        #for hver linje i filen lager jeg et nytt Sang-objekt og legger det til spillelisten
         for linje in fil:
             biter = linje.split(";")
             tittel = biter[0]
@@ -20,25 +33,33 @@ class Spilleliste:
             sanger[i] = Sang(tittel,artist)
             self.leggTilSang(sanger[i])
             i += 1
-
+   
+    #leggTilSang tar et Sang-objekt som parameter og legger det til i spillelisten
     def leggTilSang(self,sang):
         self._sanger.append(sang)
-
+  
+    #fjernSang tar et Sang-objekt som parameter og fjerner det fra spillelisten
     def fjernSang(self,sang):
         self._sanger.remove(sang)
 
+    #finnSang tar en sangtittel som parameter og returnerer sangen dersom den er i spillelisten,
+    #om den ikke er i listen returneres None
     def finnSang(self,tittel):
         for sang in self._sanger:
             if sang.sjekkTittel(tittel):
                 return sang
 
+    #spillSang bruker __str__ metoden til Sang objektet for å printe det til terminalen
     def spillSang(self,sang):
         print(sang)
 
+    #spillAlle printer alle sangene
     def spillAlle(self):
         for sang in self._sanger:
             print(sang)
 
+    #hentArtistUtvalg tar et artistNavn som parameter og lager en ny liste med alle
+    #sangene til den artisten som er i spillelisten og returnerer den
     def hentArtistUtvalg(self,artistNavn):
         nySangliste=[]
         for sang in self._sanger:
